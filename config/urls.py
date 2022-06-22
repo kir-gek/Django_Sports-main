860639ef2ec669fd224eb9646c4fa1e0c12289f7
kir-gek <Kirokronos@gmail.com>
2022-06-22 13:17:00 +0300

    sentry

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,11 @@ router.register('team', TeamViewSet)
 router.register('game', GameViewSet)
 router.register('user', UserViewSet)
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
